019a76599704c6afbf0df0ae70fdadc304732bd8
Félix Saparelli <felix@passcod.name>
2014-03-30 12:41:41 +1300

    Update !badsums to new format

--- plugins/badsums.rb
+++ plugins/badsums.rb
@@ -1,16 +1,49 @@
+require 'time'
+
 class Caskbot::Plugins::Badsums
   include Cinch::Plugin
+  include ActionView::Helpers::DateHelper
 
   match 'badsums'
   @@commands = ['badsums']
 
   def execute(m)
-    failed = 'https://dl.dropboxusercontent.com/u/17915390/CaskDLFailed.txt'
-    badsum = 'https://dl.dropboxusercontent.com/u/17915390/CaskSumError.txt'
+    url = 'https://dl.dropboxusercontent.com/u/17915390/CaskTasting.txt'
 
-    nfail = Typhoeus.get(failed).body.split.length
-    nbads = Typhoeus.get(badsum).body.split.length
-    m.reply "#{nfail} failed downloads and #{nbads} bad checksums"
-    m.reply "Details at: http://bit.ly/1hVqdXG"
+    summary =  Typhoeus.get(url).body.split("\n")
+    start_date = summary.shift.match(/(?:at\s)(.+)/)[1]
+    finish_date = summary.pop.match(/(?:at\s)(.+)/)[1]
+
+    begin
+      start_date = DateTime.parse start_date
+      finish_date = DateTime.parse finish_date
+    rescue
+      start_date = finish_date = DateTime.now
+    end
+    
+    nfail = 0
+    nnots = 0
+    nbads = 0
+    total = 0
+    
+    summary.each do |line|
+      unless line[0] == ' '
+        total += 1
+        nfail += 1 if line =~ /download error/
+        nnots += 1 if line =~ /no checksum/
+        nbads += 1 if line =~ /mismatch/
+      end
+    end
+
+    pfail = (nfail * 100.0 / total).round 1
+    pnots = (nnots * 100.0 / total).round 1
+    pbads = (nbads * 100.0 / total).round 1
+
+    started_ago = distance_of_time_in_words_to_now start_date
+    time_taken = distance_of_time_in_words start_date, finish_date
+
+    m.reply "#{nfail} failed downloads (#{pfail}%) and #{nbads} bad checksums (#{pbads}%) - #{nnots} w/o checksum (#{pnots}%) - #{total} casks total"
+    m.reply "Last check #{started_ago} ago, took #{time_taken} to complete"
+    m.reply "Details at: http://bit.ly/P5ggys"
   end
 end
