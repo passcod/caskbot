f0b1a79994453772cfc5bd073ddad1497185e4de
Félix Saparelli <felix@passcod.name>
2014-03-31 21:31:13 +1300

    Use new new format

--- plugins/badsums.rb
+++ plugins/badsums.rb
@@ -10,7 +10,7 @@ class Caskbot::Plugins::Badsums
   def execute(m)
     url = 'https://dl.dropboxusercontent.com/u/17915390/CaskTasting.txt'
 
-    summary =  Typhoeus.get(url).body.split("\n")
+    summary = Typhoeus.get(url).body.split("\n")
     start_date = summary.shift.match(/(?:at\s)(.+)/)[1]
     finish_date = summary.pop.match(/(?:at\s)(.+)/)[1]
 
@@ -21,28 +21,27 @@ class Caskbot::Plugins::Badsums
       start_date = finish_date = DateTime.now
     end
     
+    summary = summary.join("\n").split("\nTesting")
+    
     nfail = 0
-    nnots = 0
     nbads = 0
     total = 0
     
-    summary.each do |line|
-      unless line[0] == ' '
+    summary.each do |test|
+      if test =~ /\naudit/
         total += 1
-        nfail += 1 if line =~ /download error/
-        nnots += 1 if line =~ /no checksum/
-        nbads += 1 if line =~ /mismatch/
+        nfail += 1 if test =~ /Download failed/
+        nbads += 1 if test =~ /mismatch/
       end
     end
 
     pfail = (nfail * 100.0 / total).round 1
-    pnots = (nnots * 100.0 / total).round 1
     pbads = (nbads * 100.0 / total).round 1
 
     started_ago = distance_of_time_in_words_to_now start_date
     time_taken = distance_of_time_in_words start_date, finish_date
 
-    m.reply "#{nfail} failed downloads (#{pfail}%) and #{nbads} bad checksums (#{pbads}%) - #{nnots} w/o checksum (#{pnots}%) - #{total} casks total"
+    m.reply "#{nfail} failed downloads (#{pfail}%) and #{nbads} bad checksums (#{pbads}%) - #{total} casks total"
     m.reply "Last check #{started_ago} ago, took #{time_taken} to complete"
     m.reply "Details at: http://bit.ly/P5ggys"
   end
