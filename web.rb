59cd71518a4993b49c1163ac00a0c4222f4a05e5
Félix Saparelli <felix@passcod.name>
2014-04-05 00:59:23 +1300

    Remove /dev/null debug endpoint

--- web.rb
+++ web.rb
@@ -5,16 +5,6 @@ class Caskbot::Web < Sinatra::Base
     "Hello! I am Caskbot!"
   end
 
-  post '/dev/null' do
-    u = Gist.gist(JSON.dump(env) + "\n\n" + JSON.dump(params) + "\n\n" + request.body.read, {
-      access_token: Caskbot.config.github_token,
-      filename: request.path.gsub('/', '.') + '.txt',
-      public: false
-    })['html_url']
-    puts u
-    u
-  end
-
   post '/hookin/github' do
     if env.include? 'HTTP_X_GITHUB_EVENT'
       Caskbot::Hookins::Github.process(
